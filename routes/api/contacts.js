072dab25532cb8064b9b3dc78bd0347b76924385
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-04-27 18:14:02 +0300

    mongoose methods and mongoDB connect

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
+const isValidId = require("../../helpers/isValidId");
 
 router.get("/", ctrl.getContacts);
 
-router.get("/:contactId", ctrl.getContactById);
+router.get("/:contactId", isValidId, ctrl.getContactById);
 
 router.post("/", ctrl.postContact);
 
-router.delete("/:contactId", ctrl.deleteContact);
+router.delete("/:contactId", isValidId, ctrl.deleteContact);
 
-router.put("/:contactId", ctrl.putContact);
+router.put("/:contactId", isValidId, ctrl.putContact);
+
+router.patch("/:contactId/favorite", isValidId, ctrl.updateStatusContact);
 
 module.exports = router;
